4f2a58081e34960cc78f0d3fa734b5ff61eb0a0d
ihab ali <ihabali@live.com>
2017-06-21 16:27:06 -0400

    pushing to heroku

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -1,40 +1,54 @@
 var express = require("express");
 
 var router = express.Router();
-var burger = require("../models/burger");
+var db = require("../models/");
 
 // get route -> index
 router.get("/", function(req, res) {
-	res.redirect("/burgers");
+    res.redirect("/burgers");
 });
 
 router.get("/burgers", function(req, res) {
-// express callback response by calling burger.selectAllBurger
-burger.all(function(data) {
-// Wrapping the array of returned burgers in a object so it can be referenced inside our handlebars
-var hbsObject = {burgers: data};
-res.render("index", hbsObject);
-  });	
+    // express callback response by calling burger.selectAllBurger
+    db.Burger.findAll()
+        .then(function(dbBurger) {
+            console.log('dbBurger', dbBurger);
+            // Wrapping the array of returned burgers in a object so it can be referenced inside our handlebars
+            var hbsObject = { burger: dbBurger };
+            return res.render("index", hbsObject);
+        });
 });
 
-// post route -> back to index
+// // post route -> back to index
 router.post("/burgers/create", function(req, res) {
-// takes the request object using it as input for burger.addBurger
-burger.create(req.body.burger_name, function(result) {
-
- console.log(result);
- res.redirect("/");
-
-  });
+    //     // takes the request object using it as input for buger.addBurger
+    db.Burger.create({
+            burger_name: req.body.burger_name
+        })
+        .then(function(dbBurger) {
+            console.log('result', dbBurger);
+            res.redirect("/");
+        })
+               res.redirect("/");
 });
 
-// put route back to index
+// // put route -> back to index
 router.put("/burgers/update", function(req, res) {
-	burger.update(req.body.burger_id, function(result) {
-
-		console.log(result);
-		res.redirect("/");
-	});
+    db.Burger.update({
+        devoured: true
+    }, {
+        where: {
+            id: req.body.burger_id
+        }
+    }).then(function(dbBurger) {
+        res.redirect("/");
+    });
 });
+//         // wrapper for orm.js that using MySQL update callback will return a log to console,
+//         // render back to index with handle
+//         console.log(result);
+//         res.redirect("/");
+//     });
+// });
 
 module.exports = router;
