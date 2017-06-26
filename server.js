7890e32f4c5c5e6ce5aaa709a133b7218686145b
ihab ali <ihabali@live.com>
2017-06-26 17:34:35 -0400

    finshing

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
+var db = require("./models");
 
 var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
